6c455b70d44f627583ae81ed2b795fdee50f73cb
Joe Young <musicjoeyoung@gmail.com>
2023-02-09 10:10:28 -0500

    notes

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,25 +2,26 @@ const User = require("../models/User");
 
 const getUsers = async (req, res, next) => {
   try {
-    //query parameter
-
+    //create options object to store query parameters
     const options = {};
 
     //check if the req query is empty?
     if (Object.keys(req.query).length) {
+      //destructure sortByFirstName and limit from the the request query
       const { sortByFirstName, limit } = req.query;
 
-      //setup pagination
+      //setup pagination if limit is provided
       if (limit) options.limit = limit;
 
+      //sort results
       if (sortByFirstName)
         options.sort = {
           firstName: sortByFirstName === "asc" ? 1 : -1,
         };
     }
-
+    //get all users from DB using the options object
     const result = await User.find({}, {}, options);
-
+    //respond with a JSON representation of the result
     res.status(200).setHeader("Content-Type", "application/json").json(result);
   } catch (error) {
     throw new Error(`Error getting all users: ${error.message}`);
@@ -40,13 +41,15 @@ const createUser = async (req, res, next) => {
         .json({ success: false, error: "Username already exists" });
     }
     /*******/
+    //create new user in the DB using the info from the request body
     const user = await User.create(req.body);
+    //get a signed JWT for the new user
     const token = user.getSignedJwtToken();
-
+    //set up options object for JWT cookie
     const options = {
       expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 1000 * 60),
     };
-
+    //respond with success message, JWT, & user info
     res
       .status(200)
       .setHeader("Content-Type", "application/json")
